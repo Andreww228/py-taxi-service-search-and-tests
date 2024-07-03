f35c1342e703eb48f9686ecfa87547dd3383baec
AndrewwSSS <clash2clans1one@gmail.com>
2024-07-03 14:21:26 +0300

    solution

---
+++ tests/test_models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Driver, Car
+
+
+class ModelsTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="Audi",
+            country="United States"
+        )
+
+        self.assertEqual(str(manufacturer), "Audi United States")
+
+    def test_driver_str(self):
+        driver = get_user_model().objects.create_user(
+            username="Aboba",
+            password="<PASSWORD_1234>",
+            first_name="First",
+            last_name="Last",
+        )
+        self.assertEqual(str(driver), "(Aboba First Last)")
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(
+            name="Audi",
+            country="United States"
+        )
+        car = Car.objects.create(
+            model="RS6 Avant",
+            manufacturer=manufacturer,
+        )
+        self.assertEqual(str(car), "RS6 Avant")
